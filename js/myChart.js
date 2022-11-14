2331aaa931b07504ba11214e067686bdafc0f22b
kkh-147-17-3 <102606939+kkh-147-17-3@users.noreply.github.com>
2022-11-14 18:05:03 +0900

    add: index page added
    메인페이지

---
+++ js/myChart.js
@@ -0,0 +1,87 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+        let dataLabels = [];
+        let data = [];
+        let data2 = [];
+        for (let i = 1; i < 30; i++){
+            dataLabels[i-1] = i+"일";
+            data[i-1] = (Math.random() * 3 + 12);
+            data2[i-1] = 13;
+        }
+
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: dataLabels,
+                datasets: [
+                    {
+                    data: data,
+                    //backgroundColor: '#0770E3',
+                    backgroundColor: '#83B8F144',
+                    borderColor: '#83B8F1',
+                    borderWidth: 3,
+                    pointRadius: 0,
+                    fill: {value: 0}
+                    },
+                    // {
+                    // data: data2,
+                    // borderColor: '#83B8F1',
+                    // borderWidth: 1,
+                    // pointRadius: 0,
+                    // },
+                ]
+            },
+            options: {
+                
+                elements:{
+                    line:{
+                        tension:0.3
+                    }
+                },
+                plugins: {
+                    legend:{
+                        display: false
+                    },
+                    filler: {
+                        propagate: false,
+                    },
+                   
+                },
+                interaction: {
+                    intersect: false,
+                },
+                responsive: false,
+                scales: {
+                    x: {
+                        
+                        grid:{
+                            display: true,
+                            drawBorder : true,
+                        },
+                        ticks: {
+                            font :{
+                                size : 15,
+                            }
+                        }
+                    },
+                    y: {
+                        grid:{
+                            display: true,
+                            drawBorder : true,
+                        },
+                        min: 11,
+                        max: 16,
+                        ticks: {
+                            // Include a dollar sign in the ticks
+                            callback: function(value, index, ticks) {
+                                return '￦' + value * 10 + ',000';
+                            },
+                            font :{
+                                size : 15,
+                            }
+                        }
+                    }
+                }
+            },
+        });
+
+        let smooth = false;
